afa208a8e89313788b9f902c89770fe90e1394ac
adamkingsbury <adam.kingsbury@gmail.com>
2018-02-01 10:14:21 +0000

    Update .gitignore

--- deployment/mailu-down.sh
+++
@@ -1,3 +0,0 @@
-docker-compose down
-docker network disconnect mailu $(docker container ls --filter=ancestor=traefik -q)
-docker network rm mailu
--- deployment/mailu-up.sh
+++
@@ -1,3 +0,0 @@
-docker network create mailu
-docker network connect mailu $(docker container ls --filter=ancestor=traefik -q)
-docker-compose up -d
